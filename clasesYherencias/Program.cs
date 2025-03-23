b08ea4cd841abedc1e7f8f32db854b4366737130
Viv9NKnz <cocholitojustin07@gmail.com>
2025-03-22 19:21:38 -0500

    git commit

---
+++ clasesYherencias/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var rect = new Rectangulo();
+
+rect.Base=20;
+
+rect.Altura= 55;
+
+Console.WriteLine($"El area es : {rect.Area()} ");
+
+Console.WriteLine($"La Altura es : {rect.Perimetro()} "); 
+
+class Rectangulo {
+   public double Base { get; set; }
+   public double Altura { get; set; }
+
+public double Area() => Base * Altura;
+
+public double Perimetro() =>  2 * Base + 2 * Altura;
+}
